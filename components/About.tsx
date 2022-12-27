cb727eacc733e61d08a703b883901cc2dc77e26a
mpotane <1000100ten@gmail.com>
2022-12-27 17:13:49 +0800

    Remove react suspense to improve performance 🫗.

--- components/About.tsx
+++ components/About.tsx
@@ -1,11 +1,10 @@
 import dynamic from "next/dynamic";
-import { Suspense } from "react";
 import Contact from "./Contact";
 import Loading from "./Loading";
 import { Element } from "react-scroll";
 
 const Projects = dynamic(() => import("./Project"), {
-  suspense: true,
+  loading: () => <Loading />,
 });
 
 const About = () => {
@@ -14,13 +13,11 @@ const About = () => {
       name="about"
       className="min-h-screen w-full p-10 md:p-20 bg-base-200"
     >
-      <Suspense fallback={<Loading />}>
-        <h2 className="text-center py-12">About Me.</h2>
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-10 md:mx-5">
-          <Projects />
-          <Contact />
-        </div>
-      </Suspense>
+      <h2 className="text-center py-12">About Me.</h2>
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-10 md:mx-5">
+        <Projects />
+        <Contact />
+      </div>
     </Element>
   );
 };
