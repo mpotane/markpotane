6bbd0fac5cff95c28d271dedfbad636d232b5673
mpotane <1000100ten@gmail.com>
2023-01-01 12:26:56 +0800

    Update font optimization 🍒.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,11 +2,14 @@ import "../styles/globals.css";
 import { Inter } from "@next/font/google";
 import type { AppProps } from "next/app";
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({
+  subsets: ["latin"],
+  variable: "--font-inter",
+});
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <main className={inter.className}>
+    <main className={`${inter.variable} font-sans`}>
       <Component {...pageProps} />
     </main>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+const { fontFamily } = require("tailwindcss/defaultTheme");
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   daisyui: {
@@ -8,6 +10,9 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    fontFamily: {
+      sans: ['var(--font-inter)', ...fontFamily.sans],
+    },
     extend: {},
   },
   plugins: [require("daisyui")],
