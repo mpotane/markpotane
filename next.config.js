2c3ebcdc7eba3045d12c0e1da5213d2fae7b9b40
mpotane <1000100ten@gmail.com>
2023-01-11 20:21:41 +0800

    Remove modulelarizeImports 🧉.

--- next.config.js
+++ next.config.js
@@ -2,11 +2,6 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  modularizeImports: {
-    'react-icons': {
-      transform: 'react-icons/{{member}}',
-    },
-  },
 }
 
 module.exports = nextConfig
