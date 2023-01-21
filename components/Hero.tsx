14c0b3b103302dbf69b5595742206605b13f83f1
mpotane <1000100ten@gmail.com>
2023-01-21 11:38:03 +0800

    Add balancer for hero section ✍️.

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,4 +1,6 @@
 import { Element } from "react-scroll";
+import Balancer from "react-wrap-balancer";
+import { SiTelegram } from "react-icons/si";
 
 export default function Hero() {
   return (
@@ -11,27 +13,34 @@ export default function Hero() {
           <span className="py-1 px-4 text-sm text-primary-content font-semibold rounded-lg border-2 border-success shadow-[0_0_4px_2px] shadow-success">
             @mpotane
           </span>
-          <div className="p-4" />
-          <h1 className="text-3xl md:text-5xl font-bold pb-5">
-            Talk is cheap. Show me the code!
-          </h1>
-          <p className="leading-relaxed text-sm">
-            I&apos;m a{" "}
-            <span className="rounded-md px-1 text-primary-content bg-gradient-to-br from-accent to-primary-focus">
-              Software Engineer
-            </span>{" "}
-            with a passion for building scalable and maintainable web
-            applications.
-          </p>
-          <div className="pt-8">
+          <br />
+          <br />
+          <Balancer>
+            <h1 className="text-3xl md:text-5xl font-bold">
+              Talk is cheap. Show me the code!
+            </h1>
+            <br />
+            <p className="leading-relaxed text-sm">
+              I&apos;m a{" "}
+              <span className="rounded-md px-1 text-primary-content bg-gradient-to-br from-accent to-primary-focus">
+                Software Engineer
+              </span>{" "}
+              with a passion for building scalable and maintainable web
+              applications.
+            </p>
+          </Balancer>
+          <br />
+          <br />
+          <div className="flex justify-center items-center gap-2 text-info cursor-pointer hover:brightness-90">
             <a
-              className="underline underline-offset-4 text-info font-semibold"
+              className="font-semibold"
               href="https://t.me/mpotane"
               rel="noreferrer"
               target="_blank"
             >
               LET&apos;S CHAT!
             </a>
+            <SiTelegram />
           </div>
         </div>
       </article>
