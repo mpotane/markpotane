dd32eaf8daf6ee145480144beae46b49e76a0e68
mpotane <1000100ten@gmail.com>
2023-05-09 20:36:32 +0800

    Add metadata and image 05/09/2023

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,6 +9,43 @@ export const metadata: Metadata = {
   title: "Mark Edzel Potane",
   description:
     "Hello, I'm Mark Edzel Potane. A software engineer based in the Philippines. Passionate about building scalable and maintainable web applications.",
+  openGraph: {
+    title: "Mark Edzel Potane",
+    description:
+      "Hello, I'm Mark Edzel Potane. A software engineer based in the Philippines. Passionate about building scalable and maintainable web applications.",
+    url: "https://mpotane.com",
+    siteName: "Mark Edzel Potane",
+    images: [
+      {
+        url: "https://mpotane.com/image.png",
+        width: 1200,
+        height: 630,
+      },
+    ],
+    locale: "en-US",
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Mark Edzel Potane",
+    description:
+      "Hello I'm Mark Edzel Potane. A software engineer based in the Philippines. Passionate about building scalable and maintainable web applications.",
+    creator: "@mpotanedev",
+    images: ["https://mpotane/image.png"],
+  },
+  robots: {
+    index: false,
+    follow: true,
+    nocache: true,
+    googleBot: {
+      index: true,
+      follow: false,
+      noimageindex: true,
+      "max-video-preview": -1,
+      "max-image-preview": "large",
+      "max-snippet": -1,
+    },
+  },
 };
 
 export default function Page() {
