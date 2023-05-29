1abef56e67466263cfa23b5ffbe9954eea29eafc
mpotane <1000100ten@gmail.com>
2023-05-29 16:44:35 +0800

    Github for now

--- src/components/TechStack.tsx
+++ src/components/TechStack.tsx
@@ -4,7 +4,7 @@ import {
   SiReact,
   SiTailwindcss,
   SiTypescript,
-  SiAmazonaws,
+  SiGithub,
 } from "react-icons/si";
 
 export default function TeckStack() {
@@ -55,10 +55,10 @@ export default function TeckStack() {
           </div>
           <div className="p-[1px] rounded-xl w-full bg-gradient-to-br from-pink-500 to-yellow-500">
             <div className="px-5 py-10 w-full rounded-xl flex flex-wrap flex-col gap-2 justify-center items-center bg-base-300">
-              <div className="text-orange-400">
-                <SiAmazonaws size={40} />
+              <div className="text-white">
+                <SiGithub size={40} />
               </div>
-              AmazonWS
+              GitHub
             </div>
           </div>
         </div>
