abc30a69d3aef87f64711e2a5b08633e9356bf6a
mpotane <65337802+mpotane@users.noreply.github.com>
2023-08-13 10:54:01 +0800

    add option for fetch

--- src/lib/client.ts
+++ src/lib/client.ts
@@ -12,7 +12,7 @@ export const { getClient } = registerApolloClient(() => {
       uri: "https://api.github.com/graphql",
       // you can disable result caching here if you want to
       // (this does not work if you are rendering your page with `export const dynamic = "force-static"`)
-      // fetchOptions: { cache: "no-store" },
+      fetchOptions: { cache: "no-store" },
       headers: {
         authorization: `Bearer ${process.env.GITHUB_TOKEN}`,
       },
