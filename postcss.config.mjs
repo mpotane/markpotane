b3c452b61fb0213f0453b3b639cb56d2353e950a
mpotane <65337802+mpotane@users.noreply.github.com>
2023-09-20 20:25:07 +0800

    use ESM 👓

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,6 +1,8 @@
-module.exports = {
+const Postcss = {
   plugins: {
     tailwindcss: {},
     autoprefixer: {},
   },
-}
+};
+
+export default Postcss;
---
+++ tailwind.config.ts
@@ -0,0 +1,24 @@
+import { fontFamily } from "tailwindcss/defaultTheme"
+import type { Config } from "tailwindcss"
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  daisyui: {
+    themes: ["dark"],
+  },
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    fontFamily: {
+      sans: ["var(--font-inter)", ...fontFamily.sans],
+    },
+    container: {
+      center: true,
+    },
+    extend: {},
+  },
+  plugins: [require("daisyui")],
+} satisfies Config;
