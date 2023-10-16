fc4e03b1a2f60d723b16b9421581d6e3201cb9db
Mark Edzel Potane <65337802+mpotane@users.noreply.github.com>
2023-10-16 20:30:09 +0800

    add tag manager (#110)
    * initial support 🍞
    
    * modify package manager 91220231129
    
    * add bun types
    
    * add type animation
    
    * --use-pnpm
    
    * sync dependencies 9292023917
    
    * Use ftb link
    
    * Fix blog link a11y color
    
    * Update data blog 3
    
    * add canonical url
    
    * update robot meta
    
    * add analytics
    
    * add gtm

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import "./globals.css";
 import { Inter } from "next/font/google";
+import Script from "next/script";
 
 const inter = Inter({ subsets: ["latin"], variable: "--font-inter" });
 
@@ -10,6 +11,13 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" data-theme="dark">
+      <Script
+        async
+        src="https://www.googletagmanager.com/gtag/js?id=G-HGNW1LPS20"
+        strategy="afterInteractive"
+      />
+      <Script src="./script.js" strategy="afterInteractive" />
+      <Script src="./tag.js" strategy="afterInteractive" />
       <body className={`${inter.variable} font-sans`}>{children}</body>
     </html>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,9 @@ export const metadata: Metadata = {
   description:
     "Hello, I'm Mark Edzel Potane. A software engineer based in the Philippines. Passionate about building scalable and maintainable web applications.",
   metadataBase: new URL("https://mpotane.com"),
+  alternates: {
+    canonical: "/",
+  },
   openGraph: {
     title: "Mark Edzel Potane",
     description:
@@ -33,12 +36,24 @@ export const metadata: Metadata = {
   robots: {
     index: true,
     follow: true,
-    noarchive: true,
-    noimageindex: true,
-    notranslate: true,
   },
 };
 
 export default function Page() {
-  return <Container />;
+  return (
+    <>
+      <noscript>
+        <iframe
+          src="https://www.googletagmanager.com/ns.html?id=GTM-N2L95B2T"
+          height="0"
+          width="0"
+          style={{
+            display: "none",
+            visibility: "hidden",
+          }}
+        ></iframe>
+      </noscript>
+      <Container />
+    </>
+  );
 }
