5994d4c4a19e20c1a672926353f0e41930e13fb1
mpotane <65337802+mpotane@users.noreply.github.com>
2023-12-11 23:15:01 +0800

    Remove enableSystem prop from ThemeProvider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -74,7 +74,6 @@ export default function RootLayout({
         <ThemeProvider
           attribute="class"
           defaultTheme="dark"
-          enableSystem
           disableTransitionOnChange
         >
           {children}
