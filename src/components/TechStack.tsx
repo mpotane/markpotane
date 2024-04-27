a768336f45f56ce43753786dd4e8096b038d257d
mpotane <65337802+mpotane@users.noreply.github.com>
2024-04-27 11:12:55 +0800

    Update TechStack component with new icons and colors

--- src/components/TechStack.tsx
+++ src/components/TechStack.tsx
@@ -1,10 +1,10 @@
 import { TbBrandNextjs } from "react-icons/tb";
 import {
   SiReact,
-  SiTailwindcss,
-  SiTypescript,
-  SiVercel,
+  SiGoogleanalytics,
   SiSemrush,
+  SiMeta,
+  SiGoogleads,
 } from "react-icons/si";
 import { Card } from "./ui/card";
 
@@ -16,6 +16,30 @@ export default function TeckStack() {
       </div>
       <Card className="flex flex-wrap justify-start items-center gap-2 p-5">
         <div className="flex flex-col justify-center items-center w-32 h-20">
+          <div className="text-orange-300">
+            <SiSemrush size={25} />
+          </div>
+          Semrush
+        </div>
+        <div className="flex flex-col justify-center items-center w-32 h-20">
+          <div className="text-blue-500">
+            <SiMeta size={25} />
+          </div>
+          Meta Ads
+        </div>
+        <div className="flex flex-col justify-center items-center w-32 h-20">
+          <div className="text-yellow-300">
+            <SiGoogleanalytics size={25} />
+          </div>
+          GA4
+        </div>
+        <div className="flex flex-col justify-center items-center w-32 h-20">
+          <div className="text-blue-300">
+            <SiGoogleads size={25} />
+          </div>
+          Google Ads
+        </div>
+        <div className="flex flex-col justify-center items-center w-32 h-20">
           <div className="text-white">
             <TbBrandNextjs size={25} />
           </div>
@@ -26,30 +50,6 @@ export default function TeckStack() {
             <SiReact size={25} />
           </div>
           Reactjs
-        </div>
-        <div className="flex flex-col justify-center items-center w-32 h-20">
-          <div className="text-orange-300">
-            <SiSemrush size={25} />
-          </div>
-          Semrush
-        </div>
-        <div className="flex flex-col justify-center items-center w-32 h-20">
-          <div className="text-blue-500">
-            <SiTypescript size={25} />
-          </div>
-          TypeScript
-        </div>
-        <div className="flex flex-col justify-center items-center w-32 h-20">
-          <div className="text-emerald-600">
-            <SiTailwindcss size={25} />
-          </div>
-          TailwindCSS
-        </div>
-        <div className="flex flex-col justify-center items-center w-32 h-20">
-          <div className="text-white">
-            <SiVercel size={25} />
-          </div>
-          Vercel
         </div>
       </Card>
     </section>
